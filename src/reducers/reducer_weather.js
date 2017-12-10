c35ce639da1e12e5ac65d68f61b5ee0ec682c281
kameron johnson <kameronjohnson@gmail.com>
2017-12-09 18:19:15 -0800

    reducer_weather returns payload in new array

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,14 @@
-export default function(state = null, action) {
-    console.log("Action:", action);
+import { FETCH_WEATHER } from "../actions/index";
 
+//default state for this reducer is an array because we'll have multiple cities
+export default function(state = [], action) {
+    // console.log("Action received:", action);
+    switch (action.type) {
+    case FETCH_WEATHER:
+        // concat instead of push so state isn't mutated.
+        // return state.concat([action.payload.data]);
+        // or es6 way
+        return [ action.payload.data, ...state ]; // return [city,city,city] not [city, [city,city]]
+    }
     return state;
 }
