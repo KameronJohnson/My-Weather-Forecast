3039b497caeb9e877a1a00d05b6fc751f6b67fc5
kameron johnson <kameronjohnson@gmail.com>
2017-12-11 14:42:03 -0800

    update title style

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,10 @@ import WeatherList from '../containers/weather-list';
 export default class App extends Component {
     render() {
         return (
-            <div>My Weather Forecast
+            <div>
+                <h3 className="title">My Weather Forecast
+                    <small className="text-muted">  (Five Day Average for US Cities)</small>
+                </h3>
                 <SearchBar />
                 <WeatherList />
             </div>
